deeae9a20f4df586642a6bf20c4ada8bfa221d00
skatbr <96581187+skatbr@users.noreply.github.com>
2022-04-18 15:00:51 +0200

    Update PH_AIMBOT.lua

--- PH_AIMBOT.lua
+++ PH_AIMBOT.lua
@@ -9,46 +9,46 @@ local uis = game:GetService("UserInputService")
 
 
 for _, v in pairs(getgc(true)) do
-    if type(v) == "table" and type(rawget(v, 'getbodyparts')) == 'function' then
-        getbody = v
-    end
+  if type(v) == "table" and type(rawget(v, 'getbodyparts')) == 'function' then
+    getbody = v
+  end
 end
 
 if not getgenv().aim_smooth then
-	getgenv().aim_smooth = 2
+  getgenv().aim_smooth = 2
 end
 
 getgenv().aim_at = "head"
 
 function CheckRay(from,to)
-    local ray = Ray.new(from.Position,CFrame.new(from.Position,to.Position).LookVector.Unit*(from.Position-to.Position).Magnitude)
-	local part,pos = workspace:FindPartOnRayWithIgnoreList(ray, {game.Players.LocalPlayer.Character})
-	if part.Name == "Head" then
-		return true
-	elseif not part then
-		return false
-	end
+  local ray = Ray.new(from.Position,CFrame.new(from.Position,to.Position).LookVector.Unit*(from.Position-to.Position).Magnitude)
+  local part,pos = workspace:FindPartOnRayWithIgnoreList(ray, {game.Players.LocalPlayer.Character})
+  if part.Name == "Head" then
+    return true
+  elseif not part then
+    return false
+  end
 end
 
 local function closestPlayer(fov)
-   local target = nil
-   local closest = fov or math.huge
-   for i,v in ipairs(players:GetPlayers()) do
-        local character = getbody.getbodyparts(v)
-        if character and client.Character and v ~= client and v.TeamColor ~= client.TeamColor  then
-           local _, onscreen = camera:WorldToScreenPoint(character.head.Position)
-           if onscreen then
-               local targetPos = camera:WorldToViewportPoint(character.head.Position)
-               local mousePos = camera:WorldToViewportPoint(mouse.Hit.p)
-               local dist = (Vector2.new(mousePos.X, mousePos.Y) - Vector2.new(targetPos.X, targetPos.Y)).magnitude
-               if dist < closest and CheckRay(game.Players.LocalPlayer.Character.Head,character.head) then
-                    closest = dist
-                    target = v
-               end
-           end
-       end
+  local target = nil
+  local closest = fov or math.huge
+  for i,v in ipairs(players:GetPlayers()) do
+    local character = getbody.getbodyparts(v)
+    if character and client.Character and v ~= client and v.TeamColor ~= client.TeamColor  then
+      local _, onscreen = camera:WorldToScreenPoint(character.head.Position)
+      if onscreen then
+        local targetPos = camera:WorldToViewportPoint(character.head.Position)
+        local mousePos = camera:WorldToViewportPoint(mouse.Hit.p)
+        local dist = (Vector2.new(mousePos.X, mousePos.Y) - Vector2.new(targetPos.X, targetPos.Y)).magnitude
+        if dist < closest and CheckRay(game.Players.LocalPlayer.Character.Head,character.head) then
+          closest = dist
+          target = v
+        end
+      end
     end
-   return target
+  end
+  return target
 end
 
 local function aimAt(pos,smooth)
@@ -59,15 +59,15 @@ end
 
 local isAiming = false
 uis.InputBegan:Connect(function(input)
-   if input.UserInputType == Enum.UserInputType.MouseButton2 then isAiming = true end
+if input.UserInputType == Enum.UserInputType.MouseButton2 then isAiming = true end
 end)
 uis.InputEnded:Connect(function(input)
-   if input.UserInputType == Enum.UserInputType.MouseButton2 then isAiming = false end
+if input.UserInputType == Enum.UserInputType.MouseButton2 then isAiming = false end
 end)
 
 rs.RenderStepped:connect(function()
-    local t = closestPlayer(800)
-    if isAiming and t then
-           aimAt(getbody.getbodyparts(t)[getgenv().aim_at].Position, getgenv().aim_smooth)
-    end
+local t = closestPlayer(800)
+if isAiming and t then
+  aimAt(getbody.getbodyparts(t)[getgenv().aim_at].Position, getgenv().aim_smooth)
+end
 end)
