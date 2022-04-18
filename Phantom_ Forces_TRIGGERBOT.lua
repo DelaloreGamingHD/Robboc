64f586b52388111b8fb2b210a47b3a14515fee90
skatbr <96581187+skatbr@users.noreply.github.com>
2022-04-18 15:53:36 +0200

    Update Phantom_ Forces_TRIGGERBOT.lua
    fixed some bugs and added reaction time

--- Phantom_ Forces_TRIGGERBOT.lua
+++ Phantom_ Forces_TRIGGERBOT.lua
@@ -17,16 +17,21 @@ end
 if not getgenv().releasetime then
     getgenv().releasetime = 0
 end
+if not getgenv().reaction_time then
+    getgenv().reaction_time = 0
+end
 
 game:GetService("RunService").RenderStepped:Connect(function()
     if getgenv().triggerBot == true and game:GetService("Players").LocalPlayer.Character then
         if getgenv().head_check == false then
             if mouse.Target.Parent:FindFirstChild("Head") and mouse.Target.Parent.Name == "Player" and mouse.Target.Parent.Parent.Name ~= "DeadBody" then
+                wait(getgenv().reaction_time)
                 mouse1press()
                 wait(getgenv().releasetime)
                 mouse1release()
             end
         elseif mouse.Target.Name == "Head" and mouse.Target.Parent.Name == "Player" and mouse.Target.Parent.Parent.Name ~= "DeadBody"and getgenv().head_check == true then
+            wait(getgenv().reaction_time)
             mouse1press()
             wait(getgenv().releasetime)
             mouse1release()
