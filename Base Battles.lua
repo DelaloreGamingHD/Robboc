d21be1d5c47e08028bf325c601684862f9167a7c
skatbr <96581187+skatbr@users.noreply.github.com>
2022-07-10 18:56:17 +0200

    Update Base Battles.lua

--- Base Battles.lua
+++ Base Battles.lua
@@ -14,78 +14,9 @@ local b = w:CreateFolder("Gun")
 local player = game:GetService("Players").LocalPlayer
 
 
-local net = require(game:GetService("ReplicatedStorage").Libraries.Global)
-local GetTeam = function(v) 
-    return net.Teams[v]
-end
 
 
 
---thx to e621
-function DoESP()
-    if not getgenv() then
-        game.Players.LocalPlayer:Kick("your exploit 100% will not support this damn, bye!")
-    end
-    getgenv().colors = {
-        ["r"] = 255,
-        ["g"] = 255,
-        ["b"] = 255
-    }
-    getgenv().names = true
-    function WTS(part)
-        local screen = workspace.CurrentCamera:WorldToViewportPoint(part.Position)
-        return Vector2.new(screen.x, screen.y)
-    end
-    
-    function ESP(part, text, color)
-        local name = Drawing.new("Text")
-        name.Text = text
-        name.Color = color
-        name.Position = WTS(part)
-        name.Size = 20.0
-        name.Outline = true
-        name.Center = true
-        name.Visible = true
-        
-        local lol = game:GetService("RunService").Stepped:connect(function()
-            pcall(function()
-                if not getgenv().names then
-                    name:Remove()
-                    lol:Disconnect()
-                end
-                local destroyed = not part:IsDescendantOf(workspace)
-                if destroyed and name ~= nil then
-                    name:Remove()
-                    lol:Disconnect()
-                end
-                if part ~= nil then
-                    name.Position = WTS(part)
-                end
-                local _, screen = workspace.CurrentCamera:WorldToViewportPoint(part.Position)
-                if screen then
-                    name.Visible = true
-                else
-                    name.Visible = false
-                end
-            end)
-        end)
-    end
-    for i,v in pairs(game.Players:GetPlayers()) do
-        if i~=1 and v.Character and v.Character:FindFirstChild("Head") and GetTeam(v) ~= GetTeam(game.Players.LocalPlayer) then
-            ESP(v.Character.Head,v.Name,Color3.new(1, 0, 0))
-        end
-        v.CharacterAdded:Connect(function(c)
-            repeat task.wait() until c:FindFirstChild("Head") and GetTeam(v) == GetTeam(game.Players.LocalPlayer)
-            ESP(c.Head,v.Name,Color3.new(0.082352, 1, 0))
-        end)
-    end
-    game.Players.PlayerAdded:Connect(function(p)
-        p.CharacterAdded:Connect(function(c)
-            task.wait()
-            ESP(c.Head,p.Name,Color3.new(getgenv().colors["r"]/255,getgenv().colors["g"]/255,getgenv().colors["b"]/255))
-        end)
-    end)
-end
 
 
 --no recoil
@@ -179,7 +110,9 @@ b:Toggle(
 )
 
 b:Button("ESP", function()
-    DoESP()
+    if not getgenv().FontValue then
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/skatbr/Roblox-Releases/main/Base%20Battles-ESP.lua')))()
+    end
 end)
 
 
