392ce16ec64f87fa8853f1136d69191adfea5f92
skatbr <96581187+skatbr@users.noreply.github.com>
2022-07-23 00:24:39 +0200

    Update Base Battles-ESP.lua

--- Base Battles-ESP.lua
+++ Base Battles-ESP.lua
@@ -46,14 +46,6 @@ if getgenv().boxVis == nil then
     getgenv().boxVis = false
 end
 
-local function CycleFont()
-    if getgenv().FontValue + 1 > 3 then
-        getgenv().FontValue = 1
-    else
-        getgenv().FontValue = getgenv().FontValue + 1
-    end
-end
-
 
 local function GetPartCorners(Part)
     local Size = Part.Size * Vector3.new(1, 1.5)
@@ -90,55 +82,48 @@ local function DrawESP(plr)
                 if OnScreen and getgenv().Visibility then
                     Name.Position = Vector2.new(Vector.X, Vector.Y + math.clamp(Distance / 10, 10, 30) - 10)
                     Name.Size = math.clamp(30 - Distance / 10, 15, 30)
-                    if getgenv().useTeamColor then
-                        if plr.TeamColor == Player.TeamColor then
-                            Name.Color = Color3.fromRGB(0, 255, 26)
-                        elseif plr.TeamColor ~= Player.TeamColor then
-                            Name.Color = Color3.fromRGB(255, 0, 0)
-                        else
-                            Name.Color = Color3.fromRGB(174, 0, 255)
-                        end
-
-
+                    if plr.TeamColor == Player.TeamColor then
+                        Name.Color = Color3.fromRGB(0, 255, 26)
+                    elseif plr.TeamColor ~= Player.TeamColor then
+                        Name.Color = Color3.fromRGB(255, 0, 0)
                     else
-                        Name.Color = Color3.fromHSV(math.clamp(Distance / 5, 0, 125) / 255, 0.75, 1)
+                        Name.Color = Color3.fromRGB(174, 0, 255)
                     end
                     Name.Visible = true
-                    Name.Font = getgenv().FontValue
+                    Name.Font = 1
                 else
                     Name.Visible = false
                 end
 
                 Name.Text = string.format(plr.Name.." ["..tostring(math.floor(Distance*0.28)).."m]")
-
-                local PartCorners = GetPartCorners(plr.Character.HumanoidRootPart)
-                local VectorTR, OnScreenTR = Camera:WorldToScreenPoint(PartCorners.TR)
-                local VectorBR, OnScreenBR = Camera:WorldToScreenPoint(PartCorners.BR)
-                local VectorTL, OnScreenTL = Camera:WorldToScreenPoint(PartCorners.TL)
-                local VectorBL, OnScreenBL = Camera:WorldToScreenPoint(PartCorners.BL)
-
-                if (OnScreenBL or OnScreenTL or OnScreenBR or OnScreenTR) and getgenv().Visibility and getgenv().boxVis then
-                    Box.PointA = Vector2.new(VectorTR.X, VectorTR.Y + 36)
-                    Box.PointB = Vector2.new(VectorTL.X, VectorTL.Y + 36)
-                    Box.PointC = Vector2.new(VectorBL.X, VectorBL.Y + 36)
-                    Box.PointD = Vector2.new(VectorBR.X, VectorBR.Y + 36)
-                    if getgenv().useTeamColor then
-                        if plr.TeamColor == Player.TeamColor then
-                            Box.Color = Color3.fromRGB(0, 255, 26)
-                        elseif plr.TeamColor ~= Player.TeamColor then
-                            Box.Color = Color3.fromRGB(255, 0, 0)
+                if getgenv().boxVis then
+                    local PartCorners = GetPartCorners(plr.Character.HumanoidRootPart)
+                    local VectorTR, OnScreenTR = Camera:WorldToScreenPoint(PartCorners.TR)
+                    local VectorBR, OnScreenBR = Camera:WorldToScreenPoint(PartCorners.BR)
+                    local VectorTL, OnScreenTL = Camera:WorldToScreenPoint(PartCorners.TL)
+                    local VectorBL, OnScreenBL = Camera:WorldToScreenPoint(PartCorners.BL)
+                    if (OnScreenBL or OnScreenTL or OnScreenBR or OnScreenTR) and getgenv().Visibility then
+                        Box.PointA = Vector2.new(VectorTR.X, VectorTR.Y + 36)
+                        Box.PointB = Vector2.new(VectorTL.X, VectorTL.Y + 36)
+                        Box.PointC = Vector2.new(VectorBL.X, VectorBL.Y + 36)
+                        Box.PointD = Vector2.new(VectorBR.X, VectorBR.Y + 36)
+                        if getgenv().useTeamColor then
+                            if plr.TeamColor == Player.TeamColor then
+                                Box.Color = Color3.fromRGB(0, 255, 26)
+                            elseif plr.TeamColor ~= Player.TeamColor then
+                                Box.Color = Color3.fromRGB(255, 0, 0)
+                            else
+                                Box.Color = Color3.fromRGB(174, 0, 255)
+                            end
                         else
-                            Box.Color = Color3.fromRGB(174, 0, 255)
+                            Box.Color = Color3.fromHSV(math.clamp(Distance / 5, 0, 125) / 255, 0.75, 1)
                         end
-
+    
+                        Box.Thickness = math.clamp(3 - (Distance / 100), 0, 3)
+                        Box.Visible = true
                     else
-                        Box.Color = Color3.fromHSV(math.clamp(Distance / 5, 0, 125) / 255, 0.75, 1)
+                        Box.Visible = false
                     end
-
-                    Box.Thickness = math.clamp(3 - (Distance / 100), 0, 3)
-                    Box.Visible = true
-                else
-                    Box.Visible = false
                 end
             else
                 Box.Visible = false
